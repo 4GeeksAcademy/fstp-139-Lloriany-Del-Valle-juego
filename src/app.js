bf0e485d2aa627f17e5725cbbdd1d5ee24c23fec
lloriany del valle <lorianydelvalle@hotmail.com>
2026-06-27 14:11:44 +0000

    juego casi terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  // aqui empieza mi codigo 
+
+  // reglas del juego
+  const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"]
+  };
+
+  // opciones a elegir
+  const options = ["rock", "paper", "scissors", "lizard", "spock"];
+
+  // seleccion del ordenador
+  const computerChoice = options[Math.floor(Math.random() * options.length)];
+
+  // eleccion del jugador
+  let playerChoice = prompt("Elige (rock/paper/scissors/lizard/spock):");
+
+  // logica del juego
+  console.log("Tú elegiste: " + playerChoice);
+  console.log("La computadora eligió: " + computerChoice);
+
+  if (playerChoice === computerChoice) {
+    console.log("Empate!");
+  } else if (rules[playerChoice].includes(computerChoice)) {
+    console.log("Ganaste!");
+  } else {
+    console.log("Perdiste!");
+  }
+
 };
